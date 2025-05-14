f4369f907ef4ee6577e6eb529f9966ffc752346c
behruz171 <behruzzo662@gmail.com>
2025-05-14 21:46:28 +0500

    nimadir

--- app/models.py
+++ app/models.py
@@ -125,7 +125,7 @@ class Branch(models.Model):
     email = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.name} ({self.clinic.name})"
+        return f"{self.name} ({self.clinic.name}) -------------------"
 
 
 class User(AbstractUser):
