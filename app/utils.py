c0bd98659736fff454fed98c83077411abe6ab40
behruz171 <behruzzo662@gmail.com>
2025-05-22 17:32:47 +0500

    adf

--- app/utils.py
+++ app/utils.py
@@ -13,18 +13,26 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 def send_code_email_(email):
+    print("Email:", email)
     subject = "Test Title"
     body = "Test uchun"
     em = EmailMessage()
+    print(222, )
     em["Message-ID"] = make_msgid()
     em["From"] = EMAIL_HOST_USER
     em["To"] = email
     em["Subject"] = subject
+    print(333, )
     em.set_content(body, subtype="html")
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(EMAIL_HOST, 465, context=context) as smtp:
+    # context = ssl.create_default_context()
+    print(444, )
+    # Send the email
+    with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT) as smtp:
+        print(555, )
         smtp.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+        print(666, )
         smtp.sendmail(EMAIL_HOST_USER, email, em.as_string())
+        print(777, )
 
 
 
