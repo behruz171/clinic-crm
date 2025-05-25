0c2af00d297355b9e524d878837febe964d2a42b
behruz171 <behruzzo662@gmail.com>
2025-05-25 12:49:00 +0500

    nimadir

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,7 @@ import smtplib
 import ssl
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 2525
+EMAIL_PORT = 25
 EMAIL_HOST_USER = 'starclaudsuport@gmail.com'  # Gmail pochtangiz
 EMAIL_HOST_PASSWORD = 'yhvqsewlnwwqvsco'  # Gmail app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
@@ -17,23 +17,21 @@ def send_code_email_(email):
     subject = "Test Title"
     body = "Test uchun"
     em = EmailMessage()
-    print(222, )
+    print(222)
     em["Message-ID"] = make_msgid()
     em["From"] = EMAIL_HOST_USER
     em["To"] = email
     em["Subject"] = subject
-    print(333, )
+    print(333)
     em.set_content(body, subtype="html")
-    # context = ssl.create_default_context()
-    print(444, )
-    # Send the email
-    with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT) as smtp:
-        print(555, )
+    print(444)
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as smtp:
+        smtp.starttls()  # ⚠️ Muhim
+        print(555)
         smtp.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-        print(666, )
+        print(666)
         smtp.sendmail(EMAIL_HOST_USER, email, em.as_string())
-        print(777, )
-
+        print(777)
 
 
 send_code_email_("behruzzo662@gmail.com")
