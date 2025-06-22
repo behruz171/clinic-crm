28a02f0552179026a1631ff87223101d02516fa9
behruz171 <behruzzo662@gmail.com>
2025-06-22 12:27:57 +0500

    nimadir

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -3,3 +3,4 @@ from .models import *  # Custom admin interfeysini import qilish
 admin.site.register(SubscriptionPlan)
 admin.site.register(ClinicSubscription)
 admin.site.register(ApiIssue)
+admin.site.register(InactiveClinic)
